b247bd647aa165485eb09a1968aec0abad7e2f0e
alexbrenes999 <116224156+alexbrenes999@users.noreply.github.com>
2023-04-15 10:47:37 -0400

    routes 3/4

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -7,12 +7,20 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include:[Category,{model:Tag,through:ProductTag}]
+  })
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  req.params.id,{
+    include:[Category,Tag]
+  }
+  .then(catData =>  res.status(200).json(catData))
+  .catch(err=>  res.status(400).json(err))
 });
 
 // create new product
@@ -25,6 +33,7 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
+ 
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
